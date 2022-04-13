9cc3f186f4177ea6ac1c53efd8b88bf9b4b08dbb
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2022-04-13 17:16:38 +0200

    program completes without error and   gives input, but its partially incorrect

--- matrix_multiply/matrix_multiply/matrix_multiply.cpp
+++ matrix_multiply/matrix_multiply/matrix_multiply.cpp
@@ -12,24 +12,37 @@ int main() {
 		std::vector<cl::Device> device;
 		cl::Context context;
 		cl::CommandQueue commandQueue;
-		// 10 righe 5 colonne
-		std::vector<float> vettoreA(50, 3);
-		// 5 righe 10 colonne 
-		std::vector<float> vettoreB(50, 3);
-		// 10 x 10 
-		std::vector<float> vettoreC(100);
+		// 3x3
+		std::vector<float> vettoreA(9, 3);
+		// 3x3
+		std::vector<float> vettoreB(9, 3);
+		// 3x3
+		std::vector<float> vettoreC(9);
+
+		cl_int result;
 
 
 		/// SETUP
 		// recupero piattaforma
-		cl::Platform::get(&platform);
+		result =	cl::Platform::get(&platform);
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR GETTING PLATFORM" << std::endl;
+			throw result;
+		}
 
 		// recupero device 
 		// TODO: CAPIRE AUTOMATICAMENTE COME PRENDERE LA GPU FISICA E NON QUELLA INTEGRATA DEL PROCESSORE
-		platform[0].getDevices(CL_DEVICE_TYPE_ALL, &device);
+		 result = platform[0].getDevices(CL_DEVICE_TYPE_GPU, &device);
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR GETTING DEVICE" << std::endl;
+			throw result;
+		}
 		for (int i = 0; i < device.size(); i++) {
 			std::cout << device[i].getInfo<CL_DEVICE_NAME>() << std::endl;
+			std::cout << device[i].getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>() << std::endl;
 		}
+
+
 
 		// il device al context	
 		context = cl::Context(device[0]);
@@ -40,58 +53,111 @@ int main() {
 
 		// CREAZIONE BUFFER E MOVIMENTO DATI IN MEMORIA
 		// NB: i buffer che contengono delle matrici sono degli array di float!!
-		cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, vettoreA.size() * sizeof(float), vettoreA.data());
-		cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, vettoreB.size() * sizeof(float), vettoreB.data());
-		cl::Buffer bufferC(context, CL_MEM_READ_ONLY, vettoreC.size() * sizeof(float), vettoreC.data());
+		cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, vettoreA.size() * sizeof(float), vettoreA.data(), &result);
+		cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, vettoreB.size() * sizeof(float), vettoreA.data(), &result);
+		cl::Buffer bufferC(context, CL_MEM_READ_ONLY, vettoreC.size() * sizeof(float), NULL, &result);
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR CREATING BUFFERS" << std::endl;
+			throw result;
+		}
+
 
 		// scrivo dati nella memoria nei buffer appena creati
-		cl::enqueueWriteBuffer(bufferA, CL_TRUE, 0, vettoreA.size() * sizeof(float), vettoreA.data(), 0, NULL);
-		cl::enqueueWriteBuffer(bufferB, CL_TRUE, 0, vettoreB.size() * sizeof(float), vettoreB.data(), 0, NULL);
+		//result = cl::enqueueWriteBuffer(bufferA, CL_TRUE, 0, vettoreA.size() * sizeof(float), vettoreA.data(), NULL, NULL);
+		//result = cl::enqueueWriteBuffer(bufferB, CL_TRUE, 0, vettoreB.size() * sizeof(float), vettoreB.data(), NULL, NULL);
+
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR ENQUEUE WRITE BUFFERS" << std::endl;
+			throw result;
+		}
+	
 
 		// recupero kernel dal file esterno
 		std::ifstream kernelFile("kernelFile.cl");
 		std::string src(std::istreambuf_iterator<char>(kernelFile), (std::istreambuf_iterator<char>()));
 
 		// creo programma usando il kernel
-		cl::Program program(context, cl::Program::Sources(1, std::make_pair(src.c_str(), src.length() + 1)));
+		cl::Program program(context, cl::Program::Sources(1, std::make_pair(src.c_str(), src.length() + 1)), &result);
+
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR CREATING PROGRAM" << std::endl;
+			throw result;
+		}
+	
 
 		// compilo il programma
-		try {
-			program.build(device);
+		result = program.build(device);
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR BUILDING PROGRAM" << std::endl;
+			throw result;
 		}
-		catch (...) {
-			std::cerr << "ERRORE " << std::endl;
-			return 1;
-		}
+			
 
 		// creo kernel
-		cl::Kernel kernelMultiply(program, "simpleMultiply");
+		cl::Kernel kernelMultiply(program, "simpleMultiply", &result);
+
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR CREATING KERNEL" << std::endl;
+			throw result;
+		}
+		
+
+		auto workGroupSize = kernelMultiply.getWorkGroupInfo<CL_KERNEL_COMPILE_WORK_GROUP_SIZE>(device[0], &result);
+
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR CALCULATE WORK GROUP SIZE" << std::endl;
+			throw result;
+		}
+
 
 		// imposto gli argomenti del kernel	
-		kernelMultiply.setArg(0, bufferC);
-		kernelMultiply.setArg(1, 5);
-		kernelMultiply.setArg(2, 10);
-		kernelMultiply.setArg(3, 10);
-		kernelMultiply.setArg(4, 5);
-		kernelMultiply.setArg(5, bufferA);
-		kernelMultiply.setArg(6, bufferB);
+		result = kernelMultiply.setArg(0, bufferC);
+		result = kernelMultiply.setArg(1, 3);
+		result = kernelMultiply.setArg(2, 3);
+		result = kernelMultiply.setArg(3, 3);
+		result = kernelMultiply.setArg(4, 3);
+		result = kernelMultiply.setArg(5, bufferA);
+		result = kernelMultiply.setArg(6, bufferB);
 
-		// creo eventuali workgroups e utilizzo se serve la memoria locale per ottimizzare esecuzione
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR SETTING ARGUMENTS" << std::endl;
+			throw result;
+		}
 
+
+		// TODO: creo eventuali workgroups e utilizzo se serve la memoria locale per ottimizzare esecuzione
+		//
+
+		size_t dim = 9;
+			
 		// eseguo il kernel
 		//NB: NDrange sono le dimensioni globali e locali
-		commandQueue.enqueueNDRangeKernel(kernelMultiply, 0, 2, NULL, NULL);
+		result = commandQueue.enqueueNDRangeKernel(kernelMultiply, cl::NullRange, cl::NDRange(9,9), cl::NullRange, NULL, NULL);
+		//clEnqueueNDRangeKernel(commandQueue, kernelMultiply, 0, &dim , NULL, NULL, NULL, NULL, NULL);
+
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR ENQUEUE KERNEL" << std::endl;
+			throw result;
+		}
 
 
 		// leggo i risultati dell'operazione e li sposto in memoria host
-		commandQueue.enqueueReadBuffer(bufferC, CL_TRUE, 0, vettoreC.size() * sizeof(float), vettoreC.data(), NULL);
+		result = commandQueue.enqueueReadBuffer(bufferC, CL_TRUE, 0, vettoreC.size() * sizeof(float), vettoreC.data(), NULL);
+
+		if (result != CL_SUCCESS) {
+			std::cerr << "ERROR ENQUEUE READ BUFFER" << std::endl;
+			throw result;
+		}
+
 
 		for (int i = 0; i < vettoreC.size(); i++) {
 			std::cout << vettoreC[i] << std::endl;
 		}
 	}
-	catch (...) {
-		std::cerr << "ERROR BIG JUICE" << std::endl;
+	catch (cl_int e){
+		
+		std::cerr << "ERRORE: " << std::endl;
+		std::cerr << e << std::endl;
 	}
 
 
